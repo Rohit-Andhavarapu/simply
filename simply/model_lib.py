7d2e82b4ccc19b419b5e38314ff5affe63def312
Rohit-Andhavarapu <rohitandhavarapu@gmail.com>
2026-02-05 15:51:10 +0530

    sign cla refresh

--- simply/model_lib.py
+++ simply/model_lib.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """All modeling components including architecture, training and inference."""
 
 import collections
